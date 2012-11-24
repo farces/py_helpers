777f551fea8d2daf5e8904ff78c78d3d4db5f8a8
Adam McKay <amckhome@tpg.com.au>
2012-11-24 17:31:25 +1100

    pep8

--- example.py
+++ example.py
@@ -1,14 +1,17 @@
-import _winreg, reghelper
+import _winreg
+import reghelper
 
-def print_args(k,i):
+
+def print_args(k, i):
     """Basic output function for callback"""
     print i
 
-key_str = _winreg.HKEY_CURRENT_USER
-subkey_str = "SOFTWARE\\test"
+if __name__ == "__main__":
+    key_str = _winreg.HKEY_CURRENT_USER
+    subkey_str = "SOFTWARE\\test"
 
-#print listing of child subkeys and finally parent key
-reghelper.ActionTree(key_str, subkey_str, print_args)
+    #print listing of child subkeys and finally parent key
+    reghelper.ActionTree(key_str, subkey_str, print_args)
 
-#delete entire registry key and all children
-reghelper.DeleteTree(key_str, subkey_str)
+    #delete entire registry key and all children
+    reghelper.DeleteTree(key_str, subkey_str)
--- reghelper.py
+++ reghelper.py
@@ -1,5 +1,5 @@
 """
-Wrapper for _winreg.DeleteKey to recursively delete a key and all 
+Wrapper for _winreg.DeleteKey to recursively delete a key and all
 subkeys. _winreg does not provide this feature and requires any key
 contain no subkeys, though values are fine.
 Usage:
@@ -14,33 +14,35 @@ Usage:
  Callback is provided two arguments, the _winreg.HKEY_* parent key
  and the string representation of the subkey to be acted on.
 """
-
 import _winreg
 
-def DeleteTree(key_str,subkey_str):
-    """Wrapper for ActionTree(key,subkey,delete callback)"""
-    ActionTree(key_str,subkey_str,lambda k,i: _winreg.DeleteKey(k,i))
 
-def ActionTree(key_str,subkey_str,cb):
+def DeleteTree(key_str, subkey_str):
+    """Wrapper for ActionTree(key,subkey,delete callback)"""
+    ActionTree(key_str, subkey_str, lambda k, i: _winreg.DeleteKey(k, i))
+
+
+def ActionTree(key_str, subkey_str, cb):
     """Perform action in callback for each child subkey, then finally
     on the parent key"""
-    _action_subkeys(key_str,subkey_str,cb)
-    cb(key_str,subkey_str)
+    _action_subkeys(key_str, subkey_str, cb)
+    cb(key_str, subkey_str)
+
 
 def _action_subkeys(key_str, subkey_str, cb):
     """Internal:
-    Recursively steps down tree, performing the provided callback for 
+    Recursively steps down tree, performing the provided callback for
     each key found, starting at the deepest child subkey."""
     l = []
-    current_key = _winreg.OpenKey(key_str,subkey_str)
+    current_key = _winreg.OpenKey(key_str, subkey_str)
     try:
-        i=0
+        i = 0
         while True:
-            next = _winreg.EnumKey(current_key,i)
-            next_str = "%s\\%s" % (subkey_str,next)
+            next = _winreg.EnumKey(current_key, i)
+            next_str = "%s\\%s" % (subkey_str, next)
             l.append(next_str)
-            _action_subkeys(key_str,next_str,cb)
-            i+=1
+            _action_subkeys(key_str, next_str, cb)
+            i += 1
     except WindowsError:
         for item in l:
-            cb(key_str,item)
+            cb(key_str, item)
